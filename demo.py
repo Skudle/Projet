3b6b8b1055ea4125b314d47073e63e772c8cbf92
Skudle <113626842+Skudle@users.noreply.github.com>
2022-12-06 21:50:57 +0100

    Add files via upload

--- demo.py
+++ demo.py
@@ -12,33 +12,32 @@ print("Apprentissage termine.")
 state = True
 state2 = True
 course_list = []
-print("hi")
 
 while state:
 
     sense.show_letter("T")
-    # Introduction enonce
-    rhasspy.text_to_speech("Énoncez votre phrase, s'il vous plait.")
-    # Réception d'une commande vocale et affichage du résultat.
+    # Introduction enoncee
+    rhasspy.text_to_speech("Enoncez votre phrase, s'il vous plait.")
+    # Reception d'une commande vocale et affichage du resultat.
     intent = rhasspy.speech_to_intent()
 
     if intent["name"] == "Course":
         while state2:
-            print("Commande course detectée. Quelle article voulez-vous ajouter dans la liste?")
+            print("Commande course detectee. Quelle article voulez-vous ajouter dans la liste?")
             article = rhasspy.speech_to_intent()
             if article["name"] == "Arret":
                 print("stop working")
                 state2 = False
                 print(course_list)
-                rhasspy.text_to_speech('Commande course arreté')
-                print('Commande course arreté')
+                rhasspy.text_to_speech('Commande course arrete')
+                print('Commande course arrete')
             elif article["name"] == "list":
                 print(course_list)
                 rhasspy.text_to_speech(str(course_list))
 
             elif article['name'] == 'Delete':
                 course_list.clear()
-                print("Liste vidée")
+                print("Liste videe")
 
             elif article['name'] == 'Ajout':
                 print(article['variables']['article'])
@@ -52,7 +51,7 @@ while state:
 
 
     # Enonce la commande vocale reçue et les variables.
-    rhasspy.text_to_speech("Vous avez lancé la commande {} avec les paramètres {}".format(intent["name"], intent["variables"]))
+    rhasspy.text_to_speech("Vous avez lance la commande {} avec les parametres {}".format(intent["name"], intent["variables"]))
 
     # Affiche la commande vocale reçue.
     sense.show_message("Commande : {}".format(intent["name"]), scroll_speed=0.07)
